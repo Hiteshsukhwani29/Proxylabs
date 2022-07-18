10f4792fd239e4d170abc6001e17e9a89b2616e3
Saurav singh <sauravsingh85079@gmail.com>
2022-07-19 00:31:25 +0530

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./Component/Navbar/Navbar";
 import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <>
+  <Navbar/>
       <LandingPage />
     </>
   );
