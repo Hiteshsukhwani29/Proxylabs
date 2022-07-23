c634a9ae7f88c515188a32d3c625486e395e71e2
Saurav singh <sauravsingh85079@gmail.com>
2022-07-24 01:52:05 +0530

    Add List Record

--- src/components/Labrecord/LabrecordItem/index.js
+++ src/components/Labrecord/LabrecordItem/index.js
@@ -1,28 +1,19 @@
-import React from 'react'
+import React from "react";
 
-export default function index() {
+export default function index({status,marks,name}) {
   return (
     <>
-    {/* <div className='flex items-center bg-white rounded-lg mx-4 my-1 px-4 py-1 grid grid-flow-col grid-cols-3'>
-      <div className='col-span-2'>Fading Channels and Mobile Communications Lab</div>
-      <div className='text-semibo
-      ld font-bold max-w-xs text-center '>Completed</div>
-      <div className='bg-temp2 rounded-lg py-1 m-1 mx-2 px-10 max-w-xs'>
-        <div>22</div>
+      <div className="flex items-center bg-white rounded-lg mx-4 my-1 px-4 py-1 ">
+        <div className="flex-1">
+          {name}
+        </div>
+        <div className="text-semibold font-bold !w-52 text-center ">
+          {status}
+        </div>
+        <div className=" bg-white mx-2 my-1  !w-52 flex items-center justify-center">
+          <span className="bg-temp2 rounded-lg px-10 py-1 ">{marks}</span>
+        </div>
       </div>
-    </div> */}
-
-    <div className='flex items-center bg-white rounded-lg mx-4 my-1 px-4 py-1 '>
-      <div className='flex flex-1'>Fading Channels and Mobile Communications Lab</div>
-      <div className='text-semibold font-bold w-52 text-center flex flex-0'>Incompleted</div>
-      <div className='bg-temp2 rounded-lg py-1 m-1 mx-2 px-10 w-30 flex flex-0'>
-      <div>22</div>
-      </div>
-    </div>
-
-    
-
-    
     </>
-  )
+  );
 }
