3c0b76e8d0b35e75d875169d1a61f69208afb0e7
Hiteshsukhwani29 <hiteshsukhwani29@gmail.com>
2022-07-24 23:00:18 +0530

    Modified navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,84 +1,46 @@
-import React,{useState}from "react";
+import React, { useState } from "react";
 import logo from "../../assets/logo.png";
-import { MdOutlinePerson } from "react-icons/md";
-import { FaCoins } from "react-icons/fa";
+import Coin from "../../assets/animation_coin.gif";
+import OutlinePerson from "../../assets/nav_profile_icon.png";
 
 function Index() {
-
-    const [coins, setcoins] = useState([])
-    const [profile, setprofile] = useState("Saurav")
+  const [coins, setcoins] = useState(100);
+  const [profile, setprofile] = useState("Hitesh");
 
   return (
-    <div>
-      <header className="bg-transparent flex justify-start items-center px-7 py-3">
-        <img
-          style={{
-            width: "274px",
-            height: "64px",
-            cursor: "pointer",
-          }}
-          src={logo}
-          alt="Logo"
-        />
-        <nav>
-          <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-            <li>
-              <a
-                href=""
-                className="block py-2 pr-4 pl-3 text-gray  bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-              >
-                Home
-              </a>
-            </li>
-            <li>
-              <a
-                href=""
-                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Institutes
-              </a>
-            </li>
-            <li>
-              <a
-                href=""
-                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Fields
-              </a>
-            </li>
-            <li>
-              <a
-                href=""
-                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                About Us
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <div className="points ml-80  flex w-28 justify-around items-center">
-          <span class="">
-            <FaCoins />
-          </span>
-          <p>{coins} coins left</p>
+    <>
+      <div className="flex items-start pt-6 px-4">
+        <img className="h-9 flex-0 mr-10 ml-5" src={logo} alt="Logo" />
+        <div className="flex text-xs sm:text-[10px] flex-1 justify-between md:text-sm lg:text-[1rem] ml-10">
+          <div className="hover:font-semibold focus:font-semibold active:font-semibold transition ease-out hover:ease-in duration-150">
+            Home
+          </div>
+          <div className="hover:font-semibold focus:font-semibold active:font-semibold transition ease-out hover:ease-in duration-150">
+            Institute
+          </div>
+          <div className="hover:font-semibold focus:font-semibold active:font-semibold transition ease-out hover:ease-in duration-150">
+            Fields
+          </div>
+          <div className="hover:font-semibold focus:font-semibold active:font-semibold transition ease-out hover:ease-in duration-150">
+            About us
+          </div>
         </div>
-
-        <a
-          href="#"
-          style={{
-            backgroundColor: "white",
-            fontWeight: "400",
-            color: "black",
-          }}
-          className=" ml-40 flex justify-around items-center w-28 p-4 h-8 text-base no-underline rounded-3xl "
-        >
-          {profile}
-          <span>
-            <MdOutlinePerson />
-          </span>
-        </a>
-      </header>
-    </div>
+        <div className="flex-1"></div>
+        <div className="flex justify-around flex-1">
+          <div className="flex-0 flex items-center">
+            <img className="h-8 w-8" src={Coin} />
+            <div className="text-sm flex font-semibold mx-2 items-center">
+              {coins}&nbsp;
+              <div className="font-normal">coins left </div>
+            </div>
+          </div>
+          <div className="bg-white flex h-min w-max px-4 py-1 rounded-full flex-0 justify-center">
+            <div>{profile}</div>
+            <img className="h-6 w-6 ml-2" src={OutlinePerson} />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
