930cceb1897df6b3ce8ddccd28880065d7d0f73c
Hiteshsukhwani29 <hiteshsukhwani29@gmail.com>
2022-08-02 23:12:07 +0530

    Add registration in login component (unfinished)

--- src/components/LoginComponent/index.js
+++ src/components/LoginComponent/index.js
@@ -1,45 +1,212 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { X } from "heroicons-react";
 import { TextField, Button } from "@mui/material";
 import db from "../../firebase";
+import { auth } from "../../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
+/**
+ * Appropriate comments are must, otherwise changes will not be merged
+ */
+
+/**
+ * @author Hitesh Sukhwani
+ */
 function Index() {
+  /**
+   * /////////////////////////////////////////
+   * Common States
+   * /////////////////////////////////////////
+   */
+
+  /**
+   * This state will store Email ID, which will be common for both signup and signin
+   */
   const [Email, setEmail] = useState("");
-  const [Password, setPassword] = useState("");
+
+  /**
+   * This state will store welocome text, which will be changed if email is found in DB
+   */
   const [welcomeText, setwelcomeText] = useState("Welcome to Proxy Labs!");
+
+  /**
+   * This state will store bool value and will be used to check if the Mail id is incorrectly formatted or not
+   * If it is true than it will show a error in Email Box,
+   * And it will modify the helper text
+   */
   const [correctMailId, setcorrectMailId] = useState(false);
   const [incorrectMailIdHelperText, setincorrectMailIdHelperText] =
     useState("");
+
+  /**
+   * This state will store a bool value
+   * If Email Id is found in DB then
+   * It will make HideEmailBox to true
+   * ShowPasswordBox to true
+   * HideNextBtn to true
+   * ShowRegistrationBox to true
+   */
   const [HideEmailBox, setHideEmailBox] = useState(false);
   const [ShowPasswordBox, setShowPasswordBox] = useState(false);
+  const [HideNextBtn, setHideNextBtn] = useState(false);
+  const [ShowRegistrationBox, setShowRegistrationBox] = useState(false);
+
+  /**
+   * /////////////////////////////////////////
+   * ////////////////////////////////////////
+   */
+
+  /**
+   * /////////////////////////////////////////
+   * Common Functions
+   * /////////////////////////////////////////
+   */
+
+  // This variable will be used for checking correct format of mail id
   var validRegex =
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
   const checkMailId = () => {
+    // If Mail Id format is correct
     if (Email.match(validRegex)) {
+      //set state to false, will be used for reseting
       setcorrectMailId(false);
       setincorrectMailIdHelperText("");
-      var userRef = db.collection("allUsers").doc(Email).get();
-      if (Email != "") {
-        userRef.then((docSnapshot) => {
-          if (docSnapshot.exists) {
-            userRef.then((snapshot) => {
-              console.log("working", snapshot.data().name);
-              setwelcomeText("Welcome " + snapshot.data().name);
-              setHideEmailBox(true);
-              setShowPasswordBox(true);
-            });
-          } else {
-            setcorrectMailId(true);
-            setincorrectMailIdHelperText("Its a new mail id");
-            setwelcomeText("Welcome to Proxy Labs!");
-          }
-        });
-      }
+      // Check whether this mail id is present in DB
+      checkForMailIdInDatabase();
     } else {
+      //set state to true, it will display error in EmailBox
       setcorrectMailId(true);
       setincorrectMailIdHelperText("Invalid mail id");
     }
   };
+
+  const checkForMailIdInDatabase = () => {
+    var userRef = db.collection("allUsers").doc(Email).get();
+    if (Email !== "") {
+      userRef.then((docSnapshot) => {
+        if (docSnapshot.exists) {
+          userRef.then((snapshot) => {
+            console.log("working", snapshot.data().name);
+            setwelcomeText("Welcome " + snapshot.data().name);
+            setHideEmailBox(true);
+            setShowPasswordBox(true);
+          });
+        } else {
+          setcorrectMailId(true);
+          setHideEmailBox(true);
+          setHideNextBtn(true);
+          setShowRegistrationBox(true);
+          setincorrectMailIdHelperText("Its a new mail id");
+          setwelcomeText("Welcome to Proxy Labs!");
+        }
+      });
+    }
+  };
+
+  /**
+   * /////////////////////////////////////////
+   * ////////////////////////////////////////
+   */
+
+  /**
+   * /////////////////////////////////////////
+   * Login States
+   * /////////////////////////////////////////
+   */
+
+  /**
+   * This State will store the password which is entered in login password box
+   */
+  const [LoginPassword, setLoginPassword] = useState("");
+
+  /**
+   * /////////////////////////////////////////
+   * ////////////////////////////////////////
+   */
+
+  /**
+   * /////////////////////////////////////////
+   * SignUp States
+   * /////////////////////////////////////////
+   */
+
+  const [FullName, setFullName] = useState("");
+  const [NewPassword, setNewPassword] = useState("");
+  const [IsPasswordSafe, setIsPasswordSafe] = useState(true);
+  const [UnsafePasswordHelpertext, setUnsafePasswordHelpertext] = useState("");
+  const [ConfirmPassword, setConfirmPassword] = useState("");
+  const [IsConfirmPasswordSame, setIsConfirmPasswordSame] = useState(true);
+  const [
+    DifferentConfirmPasswordHelpertext,
+    setDifferentConfirmPasswordHelpertext,
+  ] = useState("");
+
+  /**
+   * /////////////////////////////////////////
+   * ////////////////////////////////////////
+   */
+
+  /**
+   * /////////////////////////////////////////
+   * SignUp Functions
+   * /////////////////////////////////////////
+   */
+
+  const checkUserBeforeRegistration = () => {
+    if (NewPassword.length < 6) {
+      setIsPasswordSafe(false);
+      setUnsafePasswordHelpertext("Password cannot be less than 6 characters");
+    } else {
+      setIsPasswordSafe(true);
+      setUnsafePasswordHelpertext("");
+      if (ConfirmPassword !== NewPassword) {
+        setIsConfirmPasswordSame(false);
+        setDifferentConfirmPasswordHelpertext("Password does not match");
+      } else {
+        setIsConfirmPasswordSame(true);
+        setDifferentConfirmPasswordHelpertext("");
+        // This function will register user with email, password and add user data in firestore
+        registerWithEmailAndPassword();
+      }
+    }
+  };
+
+  const registerWithEmailAndPassword = async () => {
+    try {
+      const res = await createUserWithEmailAndPassword(
+        auth,
+        Email,
+        NewPassword
+      );
+      const user = res.user;
+      await db.collection("allUsers").doc(Email).set({
+        name: FullName,
+      });
+      await db
+        .collection("users")
+        .doc(user.uid)
+        .set({
+          uid: user.uid,
+          name: "Hitesh",
+          authProvider: "local",
+          Email,
+        })
+        .then(setShowRegistrationBox(false));
+    } catch (err) {
+      console.error(err);
+      alert(err.message);
+    }
+  };
+
+  /**
+   * /////////////////////////////////////////
+   * ////////////////////////////////////////
+   */
+
+  /**
+   * @TODO Add comments/break this in components
+   */
   return (
     <div
       className="flex flex-col w-min p-5 rounded-xl bg-white whitespace-nowrap m-auto"
@@ -86,21 +253,72 @@ function Index() {
           label="Password"
           variant="outlined"
           onChange={(e) => {
-            setPassword(e.target.value);
+            setLoginPassword(e.target.value);
           }}
-          value={Password}
+          value={LoginPassword}
         />
       ) : (
         <></>
       )}
 
-      <Button
-        className="!bg-accent !text-white !mt-6 !px-10  !py-2 !rounded-full"
-        variant="outlined"
-        onClick={checkMailId}
-      >
-        Next
-      </Button>
+      {!HideNextBtn ? (
+        <Button
+          className="!bg-accent !text-white !mt-6 !px-10  !py-2 !rounded-full"
+          variant="outlined"
+          onClick={checkMailId}
+        >
+          Next
+        </Button>
+      ) : (
+        <></>
+      )}
+      {ShowRegistrationBox ? (
+        <div className="flex flex-col mt-4">
+          <TextField
+            className="w-96"
+            id="outlined-basic"
+            label="Full Name"
+            variant="outlined"
+            onChange={(e) => {
+              setFullName(e.target.value);
+            }}
+            value={FullName}
+          />
+          <TextField
+            error={!IsPasswordSafe}
+            helperText={UnsafePasswordHelpertext}
+            className="w-96 !mt-4"
+            id="outlined-basic"
+            label="Password"
+            variant="outlined"
+            onChange={(e) => {
+              setNewPassword(e.target.value);
+            }}
+            value={NewPassword}
+          />
+          <TextField
+            error={!IsConfirmPasswordSame}
+            helperText={DifferentConfirmPasswordHelpertext}
+            className="w-96 !mt-4"
+            id="outlined-basic"
+            label="Confirm Password"
+            variant="outlined"
+            onChange={(e) => {
+              setConfirmPassword(e.target.value);
+            }}
+            value={ConfirmPassword}
+          />
+          <Button
+            className="!bg-accent !text-white !mt-6 !px-10  !py-2 !rounded-full"
+            variant="outlined"
+            onClick={checkUserBeforeRegistration}
+          >
+            Register
+          </Button>
+        </div>
+      ) : (
+        <></>
+      )}
       <div className="flex my-6">
         <div className=" bg-gray flex-1 h-[1px] mt-3"></div>
         <div className="mx-5 flex-0 text-gray">or</div>
