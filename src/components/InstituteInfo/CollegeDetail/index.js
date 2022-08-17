208668f8fff55b5088abf98a0931297e60957679
Saurav singh <sauravsingh85079@gmail.com>
2022-08-18 01:31:30 +0530

    Add Admin Detail and Students Courses Page

--- src/components/InstituteInfo/CollegeDetail/index.js
+++ src/components/InstituteInfo/CollegeDetail/index.js
@@ -13,33 +13,42 @@ export default function index() {
             className="w-full h-10 px-5 my-3 border rounded-full "
             type="text"
             placeholder="Head of Institute"
-            style={{border: "1px solid #CBCBCB"}}
+            style={{ border: "1px solid #CBCBCB" }}
           />
 
-          <CustomSelect className="h-10 !rounded-full w-full !mx-0 !text-base !px-5 !my-3 border" defaultValue={40}
-          style={{border: "1px solid #CBCBCB"}}>
-            <StyledOption  value={40} disabled>
-            Type of University
+          <CustomSelect
+            className="h-10 !rounded-full w-full !mx-0 !text-base !px-5 !my-3 border"
+            defaultValue={40}
+            style={{ border: "1px solid #CBCBCB" }}
+          >
+            <StyledOption value={40} disabled>
+              Type of University
             </StyledOption>
             <StyledOption value={10}>Ten</StyledOption>
             <StyledOption value={20}>Twenty</StyledOption>
             <StyledOption value={30}>Thirty</StyledOption>
           </CustomSelect>
 
-          <CustomSelect className="h-10 !rounded-full w-full !mx-0 !text-base !px-5 !my-3 border" defaultValue={40}
-          style={{border: "1px solid #CBCBCB"}}>
-            <StyledOption  value={40} disabled>
-            Type of Institute
+          <CustomSelect
+            className="h-10 !rounded-full w-full !mx-0 !text-base !px-5 !my-3 border"
+            defaultValue={40}
+            style={{ border: "1px solid #CBCBCB" }}
+          >
+            <StyledOption value={40} disabled>
+              Type of Institute
             </StyledOption>
             <StyledOption value={10}>Ten</StyledOption>
             <StyledOption value={20}>Twenty</StyledOption>
             <StyledOption value={30}>Thirty</StyledOption>
           </CustomSelect>
 
-          <CustomSelect className="h-10 !rounded-full w-full !mx-0 !text-base !px-5 !my-3 border " defaultValue={40}
-          style={{border: "1px solid #CBCBCB",}}>
-            <StyledOption  value={40} disabled>
-            College Course Domain
+          <CustomSelect
+            className="h-10 !rounded-full w-full !mx-0 !text-base !px-5 !my-3 border "
+            defaultValue={40}
+            style={{ border: "1px solid #CBCBCB" }}
+          >
+            <StyledOption value={40} disabled>
+              College Course Domain
             </StyledOption>
             <StyledOption value={10}>Ten</StyledOption>
             <StyledOption value={20}>Twenty</StyledOption>
@@ -50,7 +59,7 @@ export default function index() {
             className="w-full h-10 px-5 mt-8 mb-3 border rounded-full"
             type="text"
             placeholder="Address"
-            style={{border: "1px solid #CBCBCB"}}
+            style={{ border: "1px solid #CBCBCB" }}
           />
           <div className="grid grid-cols-2 mb-4 my-3">
             <div>
@@ -58,7 +67,7 @@ export default function index() {
                 className="w-full h-10 px-5  border rounded-full"
                 type="text"
                 placeholder="City"
-                style={{border: "1px solid #CBCBCB"}}
+                style={{ border: "1px solid #CBCBCB" }}
               />
             </div>
             <div>
@@ -66,7 +75,7 @@ export default function index() {
                 className="w-full h-10 px-3  border rounded-full"
                 type="text"
                 placeholder="Pincode"
-                style={{border: "1px solid #CBCBCB"}}
+                style={{ border: "1px solid #CBCBCB" }}
               />
             </div>
           </div>
@@ -74,39 +83,12 @@ export default function index() {
             className="w-full h-10 px-5 border rounded-full my-3"
             type="text"
             placeholder="State"
-            style={{border: "1px solid #CBCBCB"}}
+            style={{ border: "1px solid #CBCBCB" }}
           />
-
-          {/* <CustomSelect className="h-9 w-full px-2 border" defaultValue={40}>
-            <StyledOption value={40} disabled>
-              Select Field
-            </StyledOption>
-            <StyledOption value={10}>Ten</StyledOption>
-            <StyledOption value={20}>Twenty</StyledOption>
-            <StyledOption value={30}>Thirty</StyledOption>
-          </CustomSelect>
-
-          <CustomSelect className="h-10 w-full px-2  border" defaultValue={40}>
-            <StyledOption value={40} disabled>
-              Select Field
-            </StyledOption>
-            <StyledOption value={10}>Ten</StyledOption>
-            <StyledOption value={20}>Twenty</StyledOption>
-            <StyledOption value={30}>Thirty</StyledOption>
-          </CustomSelect>
-
-          <CustomSelect className="h-10 w-full px-2 border" defaultValue={40}>
-            <StyledOption  value={40} disabled>
-              Select Field
-            </StyledOption>
-            <StyledOption value={10}>Ten</StyledOption>
-            <StyledOption value={20}>Twenty</StyledOption>
-            <StyledOption value={30}>Thirty</StyledOption>
-          </CustomSelect> */}
         </div>
         <div className="mx-6 my-4">
           <Button
-            className="w-full h-12 !flex !justify-between  px-4 !bg-white !text-black !rounded-full"
+            className="w-full h-10 !my-3 !flex !justify-between  px-4 !bg-white !text-black !rounded-full"
             variant="contained"
             component="label"
           >
@@ -117,9 +99,107 @@ export default function index() {
             <input hidden accept="image/*" multiple type="file" />
           </Button>
 
-          <div className="rounded-lg h-96 my-4" style={{border: "1px solid #CBCBCB"}}></div>
+          <div
+            className="rounded-lg h-96 !my-8"
+            style={{ border: "1px solid #CBCBCB" }}
+          ></div>
         </div>
       </div>
+
+      {/****************************
+       * Admin Detail
+       * ******************************/}
+
+      <div>
+        <div className=" bg-gray flex-1 h-[1px] -mx-8 "></div>
+        <div className="w-full text-xl font-semibold m-4 mt-8">
+          Admin Detail
+        </div>
+        <div className="grid grid-cols-2">
+          <div className="mx-6 my-4">
+            <input
+              className="w-full h-10 px-5 my-3 border rounded-full "
+              type="text"
+              placeholder="Head of Institute"
+              style={{ border: "1px solid #CBCBCB" }}
+            />
+
+            <input
+              className="w-full h-10 px-5 my-3 border rounded-full "
+              type="text"
+              placeholder="Head of Institute"
+              style={{ border: "1px solid #CBCBCB" }}
+            />
+
+            <input
+              className="w-full h-10 px-5 my-3 border rounded-full "
+              type="text"
+              placeholder="Head of Institute"
+              style={{ border: "1px solid #CBCBCB" }}
+            />
+          </div>
+          <div className="mx-6 my-4">
+            <input
+              className="w-full h-10 px-5 my-3 border rounded-full "
+              type="text"
+              placeholder="Head of Institute"
+              style={{ border: "1px solid #CBCBCB" }}
+            />
+
+            <input
+              className="w-full h-10 px-5 my-3 border rounded-full "
+              type="text"
+              placeholder="Head of Institute"
+              style={{ border: "1px solid #CBCBCB" }}
+            />
+          </div>
+        </div>
+      </div>
+
+      {/****************************
+       * Students and Courses
+       * ******************************/}
+
+
+<div>
+<div className=" bg-gray flex-1 h-[1px] -mx-8 "></div>
+  <div className="w-full text-xl font-semibold m-4 mt-8">
+  Students and Courses
+  </div>
+  <div className="grid grid-cols-2">
+       <div className="mx-6 my-4">
+        <div className="flex items-center justify-between font-semibold h-full">
+          <div>Total number of students</div>
+          <TextField
+            className="!w-14 !text-xs"
+            variant="outlined"
+            size="small"
+            sx={{
+              "& .MuiInputBase-root": {
+                  height: 30
+              }
+            }}
+          
+          />
+          
+        </div>
+       </div>
+       <div className="mx-6 my-4">
+       <CustomSelect
+            className="h-10 !rounded-full w-full !mx-0 !text-base !px-5 !my-3 border "
+            defaultValue={40}
+            style={{ border: "1px solid #CBCBCB" }}
+          >
+            <StyledOption value={40} disabled>
+            Courses we offer
+            </StyledOption>
+            <StyledOption value={10}>Ten</StyledOption>
+            <StyledOption value={20}>Twenty</StyledOption>
+            <StyledOption value={30}>Thirty</StyledOption>
+          </CustomSelect>
+       </div>
+  </div>
+</div>
     </div>
   );
 }
--- src/components/InstituteInfo/index.js
+++ src/components/InstituteInfo/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import CollegeDetail from "./CollegeDetail"
+import AdminDetail from "./AdminDetail"
 export default function index() {
   return (
     <div>
         <CollegeDetail/>
+        <AdminDetail/>
     </div>
   )
 }
